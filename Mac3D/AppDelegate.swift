af32ed7ac3573f739b9ac628f6a25c1e481b8d4b
Nicole Olson <nicoleolson@Nicoles-iMac.local>
2019-12-10 11:36:45 -0800

    Initial Commit

---
+++ Mac3D/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Mac3D
+//
+//  Created by Nicole Olson on 12/10/19.
+//  Copyright © 2019 Nicole Olson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
